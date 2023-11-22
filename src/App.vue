c53e5659e9c81503bca672ea78bd3f089bda4e13
Realvinci <40923786+Realvinci@users.noreply.github.com>
2023-11-22 08:35:26 +0100

    Realtime weather observatory system

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MenuBar from './components/MenuBar.vue'
+import TodayData from './components/TodayData.vue'
+import ForecastData from './components/ForecastData.vue'
+import FooterBar from './components/FooterBar.vue'
+import Weather from './components/weather.vue'
+</script>
+
+<template>
+  <header>
+    <MenuBar />
+  </header>
+  <main>
+    <TodayData />
+    <ForecastData />
+  </main>
+  <footer>
+    <FooterBar />
+  </footer>
+</template>
+
+<style scoped></style>
